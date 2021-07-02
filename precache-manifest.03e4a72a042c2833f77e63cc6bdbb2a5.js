68fbf805c907b3a8c7dcb734bcddb48512dc6724
Pritam Majumder <ptmmajumder630@gmail.com>
2021-07-02 21:09:11 +0530

    Updates

---
+++ precache-manifest.03e4a72a042c2833f77e63cc6bdbb2a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc3817d0c81f8abede4b6c02fc53305",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "f1810b4bd848f38f5fa8",
+    "url": "/face-detect/static/css/2.c7a63f68.chunk.css"
+  },
+  {
+    "revision": "740f19d84dcf8f6589a4",
+    "url": "/face-detect/static/css/main.f6f90d36.chunk.css"
+  },
+  {
+    "revision": "f1810b4bd848f38f5fa8",
+    "url": "/face-detect/static/js/2.e261e659.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/face-detect/static/js/2.e261e659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740f19d84dcf8f6589a4",
+    "url": "/face-detect/static/js/main.c25fd4e2.chunk.js"
+  },
+  {
+    "revision": "fec1349e0c898c8ea613",
+    "url": "/face-detect/static/js/runtime-main.fe3b0262.js"
+  }
+]);
